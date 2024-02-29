37312f662dd2cb3845b7ad604f4cc206b030c0dd
Muttahar sheikh <153789569+muttahar123@users.noreply.github.com>
2024-03-01 01:04:09 +0500

    chap 5

---
+++ Chapter no.5/script.js
@@ -0,0 +1,63 @@
+var num1 = 3
+var num2 = num1+5
+console.log(' Sum of 3 and 5 is: '+ num2 )
+
+
+var num1 = 3
+var num2 = num1*5
+console.log(' Sum of 3 and 5 is: '+ num2 )
+
+
+var num1 = 3
+var num2 = num1-5
+console.log(' Sum of 3 and 5 is: '+ num2 )
+
+
+var num1 = 3
+var num2 = num1/5
+console.log(' Sum of 3 and 5 is: '+ num2 )
+
+
+var num1 = 3
+var num2 = num1%5
+console.log(' Sum of 3 and 5 is: '+ num2 )
+
+var thisNum = 
+ console.log(thisNum)
+ console.log(thisNum + 3)
+
+ var numq = 5
+ console.log(numq)
+
+ // Post Increment
+var num = 77;
+var newNum = num++
+
+
+console.log('num->',num)
+console.log('newNum->',newNum)
+
+
+// Pre Imcrement
+var pre = 6;
+var preNum = ++pre
+console.log('prenum->',pre)
+console.log('preNum->',preNum)
+
+
+// Post Decrement
+var pd_num = 5;
+var pd_newNum = pd_num--
+
+
+console.log('Post Num->', pd_num)
+console.log('Posy newNum->',pd_newNum)
+
+
+
+
+// Pre Decrement
+var pre = 23;
+var preNum = --pre
+console.log('prenum->',pre)
+console.log('preNum->',preNum)
